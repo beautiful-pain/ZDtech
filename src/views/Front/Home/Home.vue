a3cd47db77b35fd236a43f82fe190ee3db7c1096
zhangguo <506430706@qq.com>
2018-11-10 11:48:10 +0800

    页面修改

--- src/views/Front/Home/Home.vue
+++ src/views/Front/Home/Home.vue
@@ -1,30 +1,29 @@
 <template>
 <div>
-  <grid :show-lr-borders="false" :show-vertical-dividers="false">
-    <grid-item link="#">
-      <img slot="icon">
-    </grid-item>
-    <grid-item link="#">
-      <img slot="icon">
-    </grid-item>
-    <grid-item link="#">
-      <img slot="icon">
+  <x-header :right-options="{showMore: true}" @on-click-more="showMenus = true">智贷金管家</x-header>
+<!--   <swiper :list="SwiperList" v-model="1"></swiper> -->
+  <grid :show-lr-borders="false" :cols="4">
+    <grid-item :label="item.name" v-for="(item,index) in GirdList" :key="index">
+      <img slot="icon" src="https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg">
     </grid-item>
   </grid>
   <tabbar>
     <tabbar-item>
-      <img slot="icon">
+      <img slot="icon" src="https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun1.svg">
       <span slot="label">首页</span>
     </tabbar-item>
     <tabbar-item show-dot>
-      <img slot="icon">
+      <img slot="icon" src="https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun1.svg">
       <span slot="label">资讯</span>
     </tabbar-item>
     <tabbar-item selected>
-      <img slot="icon">
+      <img slot="icon" src="https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun1.svg">
       <span slot="label">个人中心</span>
     </tabbar-item>
   </tabbar>
+  <div v-transfer-dom>
+    <actionsheet :menus="menus" v-model="showMenus" show-cancel></actionsheet>
+  </div>
 </div>
 </template>
 <script>
@@ -36,12 +35,59 @@ import {
   Swiper,
   Grid,
   GridItem,
-  GroupTitle
+  GroupTitle,
+  XHeader,
+  Actionsheet,
+  TransferDom,
+  ButtonTab,
+  ButtonTabItem,
 } from 'vux'
 export default {
   data() {
     return {
-      value: false
+      value: false,
+      menus: {
+        menu1: 'Take Photo',
+        menu2: 'Choose from photos'
+      },
+      showMenus: false,
+      SwiperList: [{
+        url: 'javascript:',
+        img: 'https://static.vux.li/demo/1.jpg',
+        title: '送你一朵fua'
+      }, {
+        url: 'javascript:',
+        img: 'https://static.vux.li/demo/5.jpg',
+        title: '送你一次旅行',
+        fallbackImg: 'https://static.vux.li/demo/3.jpg'
+      }],
+      GirdList: [{
+          name: '信用卡',
+          img: 'https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg',
+        },
+        {
+          name: '网贷',
+          img: 'https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg',
+        }, {
+          name: '保险',
+          img: 'https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg',
+        }, {
+          name: '支付',
+          img: 'https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg',
+        }, {
+          name: '车抵贷',
+          img: 'https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg',
+        }, {
+          name: '房抵贷',
+          img: 'https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg',
+        }, {
+          name: '进度查询',
+          img: 'https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg',
+        }, {
+          name: '个人中心',
+          img: 'https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg',
+        },
+      ]
     }
   },
   methods: {},
@@ -53,7 +99,11 @@ export default {
     Swiper,
     Grid,
     GridItem,
-    GroupTitle
+    GroupTitle,
+    XHeader,
+    Actionsheet,
+    ButtonTab,
+    ButtonTabItem,
   },
   mounted() {}
 }
