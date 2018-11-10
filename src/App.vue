8017e28fb96b09c6bf8f2f46286367e5951e4b5c
zhangguo <506430706@qq.com>
2018-11-10 12:23:24 +0800

    基本页面完成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+<div id="app" style="height:100%;">
+  <view-box ref="viewBox">
+    <router-view></router-view>
+  </view-box>
+</div>
 </template>
+<script>
+import {
+  ViewBox
+} from 'vux'
+export default {
+  data() {
+    return {
 
+    }
+  },
+  components: {
+    ViewBox
+  }
+}
+</script>
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  vertical-align: middle;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    vertical-align: middle;
+}
+body,
+html {
+    height: 100%;
+    width: 100%;
+    overflow-x: hidden;
 }
 </style>
--- src/views/Front/Home/Home.vue
+++ src/views/Front/Home/Home.vue
@@ -1,12 +1,45 @@
 <template>
 <div>
-  <x-header :right-options="{showMore: true}" @on-click-more="showMenus = true">智贷金管家</x-header>
-<!--   <swiper :list="SwiperList" v-model="1"></swiper> -->
-  <grid :show-lr-borders="false" :cols="4">
-    <grid-item :label="item.name" v-for="(item,index) in GirdList" :key="index">
-      <img slot="icon" src="https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg">
-    </grid-item>
-  </grid>
+  <x-header :right-options="{showMore: true}" @on-click-more="showMenus = true" style="width:100%;position:absolute;left:0;top:0;z-index:100;">智贷金管家</x-header>
+  <swiper loop auto :list="SwiperList"></swiper>
+  <div class="title">
+    <h2>财富优选</h2>
+    <div class="title2">
+      <span>尊享财富尊选,开启梦想生活</span>
+    </div>
+  </div>
+  <group class="group">
+    <grid :show-lr-borders="false" :cols="4">
+      <grid-item :label="item.name" v-for="(item,index) in GirdList" :key="index">
+        <img slot="icon" src="https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg">
+      </grid-item>
+    </grid>
+  </group>
+  <div class="title">
+    <h2>财富优选</h2>
+    <div class="title2">
+      <span>尊享财富尊选,开启梦想生活</span>
+    </div>
+  </div>
+  <group>
+    <cell :title="1">
+      <marquee>
+        <marquee-item v-for="i in 5" :key="i" @click.native="onClick(i)">{{ 123}} {{i}}</marquee-item>
+      </marquee>
+      <marquee>
+        <marquee-item v-for="i in 5" :key="i" @click.native="onClick(i)">{{ 123}} {{i}}</marquee-item>
+      </marquee>
+      <marquee>
+        <marquee-item v-for="i in 5" :key="i" @click.native="onClick(i)">{{ 123}} {{i}}</marquee-item>
+      </marquee>
+      <marquee>
+        <marquee-item v-for="i in 5" :key="i" @click.native="onClick(i)">{{ 123}} {{i}}</marquee-item>
+      </marquee>
+      <marquee>
+        <marquee-item v-for="i in 5" :key="i" @click.native="onClick(i)">{{ 123}} {{i}}</marquee-item>
+      </marquee>
+    </cell>
+  </group>
   <tabbar>
     <tabbar-item>
       <img slot="icon" src="https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun1.svg">
@@ -41,8 +74,28 @@ import {
   TransferDom,
   ButtonTab,
   ButtonTabItem,
+  Marquee,
+  MarqueeItem,
+  ViewBox
 } from 'vux'
 export default {
+  components: {
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell,
+    Swiper,
+    Grid,
+    GridItem,
+    GroupTitle,
+    XHeader,
+    Actionsheet,
+    ButtonTab,
+    ButtonTabItem,
+    Marquee,
+    MarqueeItem,
+    ViewBox,
+  },
   data() {
     return {
       value: false,
@@ -53,13 +106,13 @@ export default {
       showMenus: false,
       SwiperList: [{
         url: 'javascript:',
-        img: 'https://static.vux.li/demo/1.jpg',
-        title: '送你一朵fua'
+        img: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vvsr72j20p00gogo2.jpg',
+        title: '智贷金管家'
       }, {
         url: 'javascript:',
-        img: 'https://static.vux.li/demo/5.jpg',
-        title: '送你一次旅行',
-        fallbackImg: 'https://static.vux.li/demo/3.jpg'
+        img: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vvsr72j20p00gogo2.jpg',
+        title: '智贷金管家',
+        fallbackImg: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vvsr72j20p00gogo2.jpg'
       }],
       GirdList: [{
           name: '信用卡',
@@ -87,37 +140,54 @@ export default {
           name: '个人中心',
           img: 'https://static-portal.jdcloud.com/jcloud/jc/2.1.0/widget/jdc-product/i/fun4.svg',
         },
-      ]
+      ],
+      marquee: [{
+        name: '信用卡',
+      }]
     }
   },
   methods: {},
-  components: {
-    Tabbar,
-    TabbarItem,
-    Group,
-    Cell,
-    Swiper,
-    Grid,
-    GridItem,
-    GroupTitle,
-    XHeader,
-    Actionsheet,
-    ButtonTab,
-    ButtonTabItem,
-  },
   mounted() {}
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 @import './index.less';
+
+.title {
+    padding: 1rem;
+    h2 {
+        text-align: left;
+        font-size: 20px;
+    }
+    .title2 {
+        text-align: left;
+        font-size: 12px;
+    }
+}
+
+.group {
+    padding: 0.5rem;
+}
+
+.vux-header .vux-header-left button,
+.vux-header .vux-header-right a,
+.vux-header .vux-header-right button,
+/deep/.vux-header .vux-header-left a {
+    color: white;
+}
+
+.vux-header {
+    background: #00b2ff;
+}
+
 .grid-center {
-  display: block;
-  text-align: center;
-  color: #666;
+    display: block;
+    text-align: center;
+    color: #666;
 }
 
 .weui-grids {
-  background-color: #fff;
+    background-color: #fff;
 }
 </style>
